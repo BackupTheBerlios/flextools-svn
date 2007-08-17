cfd21993414765bfd96799ae52222c73e062dfe0
otupman <otupman@682360df-5a36-0410-adff-e9a91ff2f339>
2007-08-17 14:59:00 +0000

    - Refactored ProcessorHelpers.getQualifiedName(ExpressionNode) back to the FlexBuilder integration project.  - Removed imports referencing FlexBuilder from ProcessorHelpers

--- plugin/trunk/src/com/dtsworkshop/flextools/builder/processors/ProcessorHelper.java
+++ plugin/trunk/src/com/dtsworkshop/flextools/builder/processors/ProcessorHelper.java
@@ -18,29 +18,11 @@
 */
 package com.dtsworkshop.flextools.builder.processors;
 
-import com.adobe.flexbuilder.codemodel.common.CMFactory;
-import com.adobe.flexbuilder.codemodel.definitions.IClass;
-import com.adobe.flexbuilder.codemodel.definitions.IDefinition;
-import com.adobe.flexbuilder.codemodel.definitions.IMetaAttributes;
-import com.adobe.flexbuilder.codemodel.internal.tree.ExpressionNode;
-import com.adobe.flexbuilder.codemodel.internal.tree.IdentifierNode;
-import com.adobe.flexbuilder.codemodel.internal.tree.MemberedNode;
-import com.adobe.flexbuilder.codemodel.internal.tree.NodeBase;
 import com.dtsworkshop.flextools.model.BuildStateType;
 import com.dtsworkshop.flextools.model.TypeReference;
 
 public class ProcessorHelper {
-
-	public static String getQualifiedName(ExpressionNode identNode) {
-		IDefinition def = identNode.getDefinition();
-		String qualifiedName = "";
-		if(def != null) {
-			qualifiedName = def.getQualifiedName();
-			
-		}
-		return qualifiedName;
-	}
-
+	
 	public static void addImport(BuildStateType buildState, String qualifiedName) {
 		TypeReference newImport = buildState.addNewImportReference();
 		newImport.setQualifiedName(qualifiedName);
